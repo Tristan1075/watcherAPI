7f1000e5ae5209194e6517efdd5ba46a68e1ae82
Tyno79 <zinedddine97@gmail.com>
2019-10-16 15:23:27 +0200

    make it better

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const app = express();
 
 
 const hostname = "127.0.0.1";
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
