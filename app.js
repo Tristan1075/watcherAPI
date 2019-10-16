1cc141742105df5e527c199863bd47878e9d4789
Zineddine <zinedddine97@gmail.com>
2019-10-16 16:47:37 +0200

    fix sur les ids dans les models + ajout de dotenv + .env + fix du app.js pour fonctionner sur heroku

--- app.js
+++ app.js
@@ -18,21 +18,33 @@ mongoose.Promise = global.Promise;
 
 
 var mongodb = 'mongodb+srv://default:default@watcher-13drk.mongodb.net/test?retryWrites=true&w=majority';
-mongoose.connect(mongodb, {useNewUrlParser: true});
+mongoose.connect(mongodb, {useNewUrlParser: true, useFindAndModify: false});
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 const userRoutes = require('./routes/userRoutes');
 const productRoutes = require('./routes/productRoutes');
 const userAddressRoutes = require('./routes/userAddressRoutes');
+<<<<<<< Updated upstream
+const sizeRoutes = require('./routes/sizeRoutes');
+const tagRoutes = require('./routes/tagRoutes');
+
+=======
 const orderRoutes = require('./routes/orderRoutes');
 const sizeRoutes = require('./routes/sizeRoute');
+>>>>>>> Stashed changes
 
 userRoutes(app);
 productRoutes(app);
 userAddressRoutes(app);
+<<<<<<< Updated upstream
+sizeRoutes(app);
+tagRoutes(app);
+
+=======
 orderRoutes(app);
 sizeRoutes(app);
+>>>>>>> Stashed changes
 
 app.listen(port, () => {
     console.log('App listening on port ' + port);
