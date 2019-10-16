14e24831919ce1901429d8275027f82bb74c2b40
Zineddine <zinedddine97@gmail.com>
2019-10-16 16:49:40 +0200

    fix app.js

--- app.js
+++ app.js
@@ -25,26 +25,18 @@ db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 const userRoutes = require('./routes/userRoutes');
 const productRoutes = require('./routes/productRoutes');
 const userAddressRoutes = require('./routes/userAddressRoutes');
-<<<<<<< Updated upstream
 const sizeRoutes = require('./routes/sizeRoutes');
 const tagRoutes = require('./routes/tagRoutes');
-
-=======
 const orderRoutes = require('./routes/orderRoutes');
-const sizeRoutes = require('./routes/sizeRoute');
->>>>>>> Stashed changes
 
 userRoutes(app);
 productRoutes(app);
 userAddressRoutes(app);
-<<<<<<< Updated upstream
 sizeRoutes(app);
 tagRoutes(app);
 
-=======
 orderRoutes(app);
 sizeRoutes(app);
->>>>>>> Stashed changes
 
 app.listen(port, () => {
     console.log('App listening on port ' + port);
