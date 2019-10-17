9f8f925b256fbe1b8f123190bb113e0c55caecc6
GautierL4 <gautier.laurent4@gmail.com>
2019-10-17 14:18:35 +0200

    Change callback with async function for order, change order model to match entity Many to Many relationship

--- src/orderController.js
+++ src/orderController.js
@@ -21,79 +21,98 @@ exports.order_create = async function(req, res){
     }
 };
 
-exports.order_pay = async function(req, res){
-    Order.findOne({_id: req.body.id_order}, function(err, order){
-        if(err) res.send(err);
+async function getProducts(order){
+    try{
+        var products = await Product.find({_id: { $in: order.products }});
+        return products;
+    }
+    catch(error){
+        res.send(error);
+    }
+}
+
+async function getProductsAmount(order){
+    try{
         var amount = 0;
-        OrderProduct.find().all('id_order', order._id, function(err, products){
-           for( let i = 0; i < products.length; i++){
-               Product.findOne({_id: products[i]._id}, function(err, product){
-                  amount += product.price;
-               });
-           }
+        var products = getProducts(order);
+        for(let i=0; i<products.length;i++){
+            amount += products[i].price;
+        }
+        return amount;
+    }
+    catch(error){
+        res.send(error);
+    }
+}
+
+exports.order_pay = async function(req, res){
+    try{
+        var order = await Order.findOne({_id: req.body.id_order});
+        var amount = await getProductsAmount(order);
+        var result = await stripe.charges.create({
+            amount: amount, // Unit: cents
+            currency: 'eur',
+            source: req.body.tokenId,
+            description: 'Test payment',
         });
-        stripe.charges.create({
-                amount: amount, // Unit: cents
-                currency: 'eur',
-                source: req.body.tokenId,
-                description: 'Test payment',
-            })
-            .then(result => res.status(200).json(result));
-    });
+        res.status(200).json(result);
+    }
+    catch(error){
+        res.send(error);
+    }
 };
 
 exports.order_total_price = async function(req, res){
-    Order.findOne({_id: req.body.id_order}, function(err, order){
-        if(err) res.send(err);
-        var amount = 0;
-        OrderProduct.find().all('id_order', order._id, function(err, products){
-            for( let i = 0; i < products.length; i++){
-                Product.findOne({_id: products[i]._id}, function(err, product){
-                    amount += product.price;
-                });
-            }
-        });
+    try{
+        var order = await Order.findOne({_id: req.body.id_order});
+        var amount = await getProductsAmount(order);
         res.json({price: amount});
-    });
+    }
+    catch(error){
+        res.send(error);
+    }
 };
 
 exports.order_products = async function(req, res){
-    Order.findOne({_id: req.body.id_order}, function(err, order){
-        if(err) res.send(err);
-        let productsArray = [];
-        OrderProduct.find().all('id_order', order._id, function(err, products){
-            for( let i = 0; i < products.length; i++){
-                Product.findOne({_id: products[i]._id}, function(err, product){
-                    productsArray.push(product);
-                });
-            }
-        });
-        res.json(productsArray);
-    });
+    try{
+        var order = await Order.findOne({_id: req.body.id_order});
+        let products = getProducts(order);
+        res.json(products);
+    }
+    catch(error){
+        res.send(error);
+    }
 };
 
 exports.order_cancel = async function(req, res){
-    Order.findOne({id: req.body.id_order}, function(err, order){
-        if(err) res.send(err);
+    try{
+        var order = await Order.findOne({id: req.body.id_order});
         if(order.state === 3 || order.state === 1){
-            Order.update({_id: order._id}, {state: 5} ,function(err, order){
-                if(err) res.send(err);
-                res.sendStatus(200);
-            })
-        }
-    });
+            await Order.update({_id: order._id}, {state: 5});
+            res.sendStatus(200);
+        };
+    }
+    catch(error){
+        res.send(error);
+    }
 };
 
 exports.order_state = async function(req, res){
-    Order.findOne({id: req.body.id_order}, function(err, order){
-       if(err) res.send(err);
-       res.json(order.state);
-    });
+    try{
+        var order = await Order.findOne({id: req.body.id_order})
+        res.json(order.state);
+    }
+    catch(error){
+        res.send(error);
+    }
 };
 
 exports.order_history = async function(req, res){
-    Order.find({id_user: req.body.id_user}, function(err, orders){
-        if(err) res.send(err);
+    try{
+        var orders = await Order.find({id_user: req.body.id_user});
         res.json(orders);
-    });
+    }
+    catch(error){
+
+    }
 };
